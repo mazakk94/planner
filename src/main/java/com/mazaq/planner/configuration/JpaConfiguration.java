21846eda3b02c35e3370089c85b2b1d5a8fcba70
Mazaq <mazakk94@gmail.com>
2017-09-06 01:36:01 +0200

    procc

--- src/main/java/com/mazaq/planner/configuration/JpaConfiguration.java
+++ src/main/java/com/mazaq/planner/configuration/JpaConfiguration.java
@@ -96,7 +96,9 @@ public class JpaConfiguration {
 	 */
 	private Properties jpaProperties() {
 		Properties properties = new Properties();
-		properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.sampleapp.hibernate.dialect"));
+//		org.hibernate.dialect.MySQLDialect
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+//		properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.sampleapp.hibernate.dialect"));
 		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("datasource.sampleapp.hibernate.hbm2ddl.method"));
 		properties.put("hibernate.show_sql", environment.getRequiredProperty("datasource.sampleapp.hibernate.show_sql"));
 		properties.put("hibernate.format_sql", environment.getRequiredProperty("datasource.sampleapp.hibernate.format_sql"));
