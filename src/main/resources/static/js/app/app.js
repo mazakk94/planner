edd00e68c33e4d2e170f0920b94b4b7e08b3c079
Mazaq <mazakk94@gmail.com>
2017-10-01 19:05:05 +0200

    merge with heroku

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -1,31 +1,31 @@
-var app = angular.module('crudApp',['ui.router','ngStorage']);
+var app = angular.module('crudApp', ['ui.router', 'ngStorage']);
 
 // app.constant('urls', {
 //     BASE: 'http://localhost:8080/PlannerApp',
-//     ROOM_SERVICE_API : 'http://localhost:8080/PlannerApp/api/room/',
-//     EVENT_SERVICE_API : 'http://localhost:8080/PlannerApp/api/event/'
+//     ROOM_SERVICE_API: 'http://localhost:8080/PlannerApp/api/room/',
+//     EVENT_SERVICE_API: 'http://localhost:8080/PlannerApp/api/event/'
 // });
 app.constant('urls', {
     BASE: 'http://powerful-brook-93779.herokuapp.com/PlannerApp',
-    ROOM_SERVICE_API : 'http://powerful-brook-93779.herokuapp.com/PlannerApp/api/room/',
-    EVENT_SERVICE_API : 'http://powerful-brook-93779.herokuapp.com/PlannerApp/api/event/'
+    ROOM_SERVICE_API: 'http://powerful-brook-93779.herokuapp.com/PlannerApp/api/room/',
+    EVENT_SERVICE_API: 'http://powerful-brook-93779.herokuapp.com/PlannerApp/api/event/'
 });
 
 app.config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
 
-        $stateProvider      
-        .state('events', {
-            url: '/',
-            templateUrl: 'partials/list',
-            controller:'EventController',
-            controllerAs:'ctrl',
-            resolve: {
-                events: function ($q, EventService, CalendarService) {
-                    console.log('Load all events');
+        $stateProvider
+            .state('events', {
+                url: '/',
+                templateUrl: 'partials/list',
+                controller: 'EventController',
+                controllerAs: 'ctrl',
+                resolve: {
+                    events: function($q, EventService, CalendarService) {
+                        console.log('Load all events');
+                    }
                 }
-            }
-        });
+            });
         $urlRouterProvider.otherwise('/');
-    }]);
-
+    }
+]);
